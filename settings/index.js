fe0834d0cf692c4973b63bd647a931b6ef70c0ef
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-11-04 12:38:18 +0100

    remove cache from localise

--- settings/index.js
+++ settings/index.js
@@ -327,8 +327,9 @@ const settings = {
         passwords : {
             'admin': '1751bfe48d5ad21fd9d'
         },
-        lang_api_endpoint : 'https://api.eventjuicer.workers.dev/?url=https%3A%2F%2Flocalise.biz%2Fapi%2Fexport%2Fall.json%3Fformat%3Dmulti%26pretty%26key%3DtWMy1RRHuiW6DD9T7AyYtFlQzAeztbBK',
-
+       
+        lang_api_endpoint : 'https://localise.biz/api/export/all.json?format=multi&key=tWMy1RRHuiW6DD9T7AyYtFlQzAeztbBK',    
+        
         api : "https://api.eventjuicer.com/v1/public/hosts/ecommerceberlin.com",
      //   proxy: "https://api.eventjuicer.workers.dev?url=",
         proxy: "",
